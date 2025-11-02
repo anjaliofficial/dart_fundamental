ed312043e6c8f205fe95d49c4fcb674dd4c4b859
anjaliofficial <bistaanjali1415@gmail.com>
2025-11-02 07:58:49 +0545

    even or odd

---
+++ lib/even_or_odd.dart
@@ -0,0 +1,9 @@
+//qn1.  wap  to check a number even or odd.
+void main() {
+  int num = 5;
+  if (num % 2 == 0) {
+    print("$num is even number");
+  } else {
+    print("$num is odd number");
+  }
+}
